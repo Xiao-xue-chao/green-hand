53794636ab689feb846f0a0b9b00ea0756ccc8da
Xiao-xue-chao <1507019139@qq.com>
2021-05-07 22:37:37 +0800

    计算机二级

---
+++ 2021-5-7/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//#include  <stdlib.h>
+//#include  <stdio.h>
+//#define   N   10
+//double fun(double* x)
+//{
+//	int  i, j;    double  av, y[N];
+//	av = 0;
+//	for (i = 0; i < N; i++)
+//		/**********found**********/
+//		av += x[i] / N;
+//	for (i = j = 0; i < N; i++)
+//		if (x[i] < av)
+//		{
+//			y[j] = x[i]; x[i] = -1;
+//			/**********found**********/
+//			j++;
+//		}
+//	i = 0;
+//	while (i < N)
+//	{
+//		if (x[i] != -1) y[j++] = x[i];
+//		/**********found**********/
+//		i++;
+//	}
+//	for (i = 0; i < N; i++)x[i] = y[i];
+//	return  av;
+//}
+//void main()
+//{
+//	int  i;     double  x[N];
+//	for (i = 0; i < N; i++) { x[i] = rand() % 50; printf("%4.0f ", x[i]); }
+//	printf("\n");
+//	printf("\nThe average is: %f\n", fun(x));
+//	printf("\nThe result :\n", fun(x));
+//	for (i = 0; i < N; i++)  printf("%5.0f ", x[i]);
+//	printf("\n");
+//}
