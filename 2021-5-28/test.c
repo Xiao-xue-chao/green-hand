e6f8b24223ab776dee2918dfc08e18e85a388f8e
Xiao-xue-chao <1507019139@qq.com>
2021-05-28 22:47:56 +0800

    每日一练

---
+++ 2021-5-28/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+int total(int a)
+{
+
+}
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	int sum = totol(a);
+	printf("%d", sum);
+	return 0;
+}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("      *\n     ***\n    *****\n   *******\n  *********\n ***********\n*************\n");
+//	printf(" ***********\n  *********\n   ********\n    *****\n     ***\n      *\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//void reverse(char* str, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i <= sz / 2; i++)
+//	{
+//		char tmp = *str;
+//		*str = *(str + sz - 1);
+//		*(str + sz - 1) = tmp;
+//		str++;
+//		sz--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[10] = "0";
+//	scanf("%s", arr);
+//	int sz = strlen(arr);
+//	reverse(arr, sz);
+//	printf("%s\n", arr);
+//	return 0;
+//}
